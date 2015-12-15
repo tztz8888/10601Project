ed63ef0919034f86b11fdf2d0377f5bd383173d6
manchiachang <manchiachang@gmail.com>
2015-12-15 17:13:18 -0500

    Revise Bug in GenerateHOGForImg

--- Manga/GenerateHOGForImg.m
+++ Manga/GenerateHOGForImg.m
@@ -4,10 +4,11 @@ function [ feat_vec] = GenerateHOGForImg( data )
     num_data = size(data,1);
     feat_vec= zeros(num_data,496);
     for i=1:num_data
+        tempdata=data(i,:);
         newImage=zeros(32,32,3,'uint8');
-        newImage(:,:,1)=reshape(data(1:1024),32,32)';
-        newImage(:,:,2)=reshape(data(1025:2048),32,32)';
-        newImage(:,:,3)=reshape(data(2049:3072),32,32)';
+        newImage(:,:,1)=reshape(tempdata(1:1024),32,32)';
+        newImage(:,:,2)=reshape(tempdata(1025:2048),32,32)';
+        newImage(:,:,3)=reshape(tempdata(2049:3072),32,32)';
         feat_vec(i,:)=extract_HOGfeature(newImage);
     end
 end
